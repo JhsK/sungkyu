8d2fc1ceeb54c07a95c6b8f0284ce19a485c476f
Insomenia <qwe6293@nate.com>
2021-10-08 14:24:15 +0900

    [feat] _app.js 공통 head 작성

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Head from 'next/head';
+import type { AppProps } from 'next/app';
 
-const App = ({ Component }) => (
+const App = ({ Component }: AppProps) => (
   <>
     <Head>
       <meta charSet="utf-8" />
