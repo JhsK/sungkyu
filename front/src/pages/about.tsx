5a411932d9791e2242f8bfb3770654648e98c165
JhsK <qwe6293@nate.com>
2021-11-21 15:56:50 +0900

    [#6] 스크롤 이벤트 커스텀 훅 생성

--- front/src/pages/about.tsx
+++ front/src/pages/about.tsx
@@ -1,22 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import AboutComponent from 'src/components/About';
 import Header from 'src/components/Header';
+import useScroll from 'src/hooks/useScroll';
 
 const About = () => {
-  const [headerPadding, setHeaderPadding] = useState(false);
-  const [logoSize, setLogoSize] = useState(60);
-
-  useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 0) {
-        setHeaderPadding(true);
-        setLogoSize(30);
-      } else if (window.scrollY <= 0) {
-        setHeaderPadding(false);
-        setLogoSize(60);
-      }
-    });
-  }, []);
+  const [headerPadding, logoSize] = useScroll(null);
 
   return (
     <>
---
+++ front/src/pages/blog.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Blog = () => <h1>test</h1>;
+
+export default Blog;
