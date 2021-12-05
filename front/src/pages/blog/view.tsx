9a99a929d598659c854bcf76628d1a6ca6b59d9f
JhsK <qwe6293@nate.com>
2021-12-05 17:30:22 +0900

    [#6] blog/post/~ -> blog/~ 경로 변경

--- front/src/pages/blog/view.tsx
+++ front/src/pages/blog/view.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 
-const PostViewer = dynamic(() => import('../../../components/Post/PostViewer'), { ssr: false });
+const PostViewer = dynamic(() => import('../../components/Blog/Post/PostViewer'), { ssr: false });
 
 const Post = () => <PostViewer />;
 
