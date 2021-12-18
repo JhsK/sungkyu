56e55ffaace16f3e9eeaf77385182896a2f918a9
JhsK <qwe6293@nate.com>
2021-12-18 22:23:14 +0900

    [#5] 프로젝트 골격 작성 및 framer 적용

--- front/src/components/Projects/index.tsx
+++ front/src/components/Projects/index.tsx
@@ -1,31 +1,124 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { GrGroup } from 'react-icons/gr';
+import { BsPerson } from 'react-icons/bs';
+import { motion } from 'framer-motion';
 
 const Container = styled.div`
   width: 1200px;
   margin: 9rem auto 0 auto;
 `;
 
-const SoloProjectsContainer = styled.div`
-  margin-bottom: 2rem;
+const ProjectsContainer = styled.div`
+  margin-bottom: 5rem;
 
-  .icons {
-    margin-right: 0.5rem;
+  .titleContainer {
+    display: flex;
+    align-items: center;
+    margin-bottom: 1rem;
+
+    .icons {
+      margin-right: 0.5rem;
+    }
+
+    .ProjectsTitle {
+      font-size: 2rem;
+      font-weight: bold;
+      line-height: 3rem;
+    }
   }
 
-  .soloProjectsTitle {
-    font-size: 2rem;
-    font-weight: bold;
+  .cardContainer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-wrap: wrap;
+
+    .card {
+      width: 23%; /* 반응형으로 적용 필요 */
+      height: 330px;
+      border-radius: 15px;
+      border: 1px solid rgba(0, 0, 0, 0.1);
+      cursor: pointer;
+
+      img {
+        width: 100%;
+        height: 170px;
+        object-fit: cover;
+        border-radius: 15px 15px 0 0;
+      }
+
+      .cardTitleContainer {
+        padding: 1rem 0 0 1rem;
+
+        span:nth-child(1) {
+          display: block;
+          font-size: 1.3rem;
+          font-weight: bold;
+          margin-bottom: 0.3rem;
+        }
+        span:nth-child(2) {
+          font-size: 1rem;
+        }
+      }
+    }
   }
 `;
 
+const tmp = [1, 2, 3, 4];
+
 const ProjectsComponent = () => (
   <Container>
-    <SoloProjectsContainer>
-      <GrGroup size="25" className="icons" />
-      <span className="soloProjectsTitle">개인 프로젝트</span>
-    </SoloProjectsContainer>
+    <ProjectsContainer>
+      <div className="titleContainer">
+        <BsPerson size="40" className="icons" />
+        <span className="ProjectsTitle">개인 프로젝트</span>
+      </div>
+      <div className="cardContainer">
+        {tmp.map((item) => (
+          <motion.div
+            className="card"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{
+              scale: 0.8,
+              rotate: -90,
+              borderRadius: '100%',
+            }}
+          >
+            <img src="test.jpg" alt="cover" />
+            <div className="cardTitleContainer">
+              <span>title</span>
+              <span>content</span>
+            </div>
+          </motion.div>
+        ))}
+      </div>
+    </ProjectsContainer>
+    <ProjectsContainer>
+      <div className="titleContainer">
+        <GrGroup size="40" className="icons" />
+        <span className="ProjectsTitle">팀 프로젝트</span>
+      </div>
+      <div className="cardContainer">
+        {tmp.map((item) => (
+          <motion.div
+            className="card"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{
+              scale: 0.8,
+              rotate: -90,
+              borderRadius: '100%',
+            }}
+          >
+            <img src="test.jpg" alt="cover" />
+            <div className="cardTitleContainer">
+              <span>title</span>
+              <span>content</span>
+            </div>
+          </motion.div>
+        ))}
+      </div>
+    </ProjectsContainer>
   </Container>
 );
 
--- front/src/pages/projects.tsx
+++ front/src/pages/projects.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from 'src/components/Footer';
 import Header from 'src/components/Header';
 import ProjectsComponent from 'src/components/Projects';
 
@@ -6,6 +7,7 @@ const Project = () => (
   <>
     <Header logoColor />
     <ProjectsComponent />
+    <Footer />
   </>
 );
 
