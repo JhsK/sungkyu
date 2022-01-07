b64578c3127246db06c0d83ffcc168e6b32bf414
JhsK <qwe6293@nate.com>
2022-01-07 22:47:53 +0900

    [#27] 쿠기,세션, passport app.js 설정

--- back/app.js
+++ back/app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const passport = require("passport");
+const session = require("express-session");
 const dotenv = require("dotenv");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 
+const userRouter = require("./routes/user");
 const db = require("./models");
+const passportConfig = require("./passport");
 
 dotenv.config();
 const app = express();
@@ -13,6 +18,7 @@ db.sequelize
     console.log("db 연결 성공");
   })
   .catch(console.error);
+passportConfig();
 
 app.use(
   cors({
@@ -24,6 +30,24 @@ app.use(
 app.use("/", express.static(path.join(__dirname, "/app/uploads")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+    proxy: true,
+    cookie: {
+      httpOnly: true,
+      secure: true,
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("hello api");
