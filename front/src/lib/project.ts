d2785c4b0bd19d040ae3eb1f95b1ea52fcf9bac5
JhsK <qwe6293@nate.com>
2022-01-27 22:26:49 +0900

    [#6] 프로젝트 마크다운 실제 프로젝트 md 파일 적용

--- front/src/lib/project.ts
+++ front/src/lib/project.ts
@@ -4,41 +4,41 @@ import path from 'path';
 import { remark } from 'remark';
 
 console.log(process.cwd());
-const postsDirectory = path.join(process.cwd(), 'posts');
+const projectsDirectory = path.join(process.cwd(), 'projects');
 
-export function getSortedPostsData() {
-  // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, '');
+// export function getSortedPostsData() {
+//   // Get file names under /posts
+//   const fileNames = fs.readdirSync(postsDirectory);
+//   const allPostsData = fileNames.map((fileName) => {
+//     // Remove ".md" from file name to get id
+//     const id = fileName.replace(/\.md$/, '');
 
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
-    // Combine the data with the id
-    return {
-      id,
-      ...matterResult.data,
-      ...matterResult.content,
-    };
-  });
-  // Sort posts by date
-  return allPostsData.sort(({ date: a }, { date: b }) => {
-    if (a < b) {
-      return 1;
-    }
-    if (a > b) {
-      return -1;
-    }
-    return 0;
-  });
-}
+//     // Read markdown file as string
+//     const fullPath = path.join(postsDirectory, fileName);
+//     const fileContents = fs.readFileSync(fullPath, 'utf8');
+//     // Use gray-matter to parse the post metadata section
+//     const matterResult = matter(fileContents);
+//     // Combine the data with the id
+//     return {
+//       id,
+//       ...matterResult.data,
+//       ...matterResult.content,
+//     };
+//   });
+//   // Sort posts by date
+//   return allPostsData.sort(({ date: a }, { date: b }) => {
+//     if (a < b) {
+//       return 1;
+//     }
+//     if (a > b) {
+//       return -1;
+//     }
+//     return 0;
+//   });
+// }
 
-export function getAllPostIds() {
-  const fileNames = fs.readdirSync(postsDirectory);
+export function getAllProjectsIds() {
+  const fileNames = fs.readdirSync(projectsDirectory);
 
   // Returns an array that looks like this:
   // [
@@ -60,15 +60,15 @@ export function getAllPostIds() {
   }));
 }
 
-export async function getPostData(id) {
-  const fullPath = path.join(postsDirectory, `${id}.md`);
+export async function getProjectsData(id) {
+  const fullPath = path.join(projectsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
 
   // Use gray-matter to parse the post metadata section
   const matterResult = matter(fileContents);
 
   // Use remark to convert markdown into HTML string
-  const processedContent = await remark().use().process(matterResult.content);
+  const processedContent = await remark().process(matterResult.content);
   const contentHtml = processedContent.toString();
 
   // Combine the data with the id and contentHtml
