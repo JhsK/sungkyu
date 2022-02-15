ff4f904a469b4eaa4d352c2ad8e80b58a17fc63d
JhsK <qwe6293@nate.com>
2022-02-15 21:37:23 +0900

    [#23] 프로젝트 메인 페이지 반응형 적용

--- front/src/components/Projects/index.tsx
+++ front/src/components/Projects/index.tsx
@@ -4,19 +4,37 @@ import { GrGroup } from 'react-icons/gr';
 import { BsPerson } from 'react-icons/bs';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
+import useDevice from 'src/hooks/useDevice';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper';
+import 'swiper/css';
 
 const Container = styled.div`
-  width: 1200px;
-  margin: 9rem auto 0 auto;
+  max-width: 1200px;
+  width: 100%;
+  margin: 5rem auto 0 auto;
+
+  @media ${(props) => props.theme.HDPC} {
+    max-width: 1000px;
+  }
+
+  @media ${(props) => props.theme.PC} {
+    max-width: 700px;
+  }
 `;
 
 const ProjectsContainer = styled.div`
   margin-bottom: 5rem;
+  width: 100%;
 
   .titleContainer {
     display: flex;
     align-items: center;
     margin-bottom: 1rem;
+
+    @media ${(props) => props.theme.TABLET} {
+      justify-content: center;
+    }
 
     .icons {
       margin-right: 0.5rem;
@@ -30,10 +48,16 @@ const ProjectsContainer = styled.div`
   }
 
   .cardContainer {
+    width: 100%;
     display: flex;
     align-items: center;
     justify-content: space-between;
     flex-wrap: wrap;
+
+    @media ${(props) => props.theme.TABLET} {
+      flex-wrap: nowrap;
+      justify-content: center;
+    }
 
     .card {
       width: 23%; /* 반응형으로 적용 필요 */
@@ -41,6 +65,18 @@ const ProjectsContainer = styled.div`
       border-radius: 15px;
       border: 1px solid rgba(0, 0, 0, 0.1);
       cursor: pointer;
+
+      @media ${(props) => props.theme.HDPC} {
+        width: 28%;
+      }
+
+      @media ${(props) => props.theme.PC} {
+        width: 40%;
+      }
+
+      @media ${(props) => props.theme.TABLET} {
+        width: 250px;
+      }
 
       img {
         width: 100%;
@@ -66,53 +102,123 @@ const ProjectsContainer = styled.div`
   }
 `;
 
-const ProjectsComponent = () => (
-  <Container>
-    <ProjectsContainer>
-      <div className="titleContainer">
-        <BsPerson size="40" className="icons" />
-        <span className="ProjectsTitle">개인 프로젝트</span>
-      </div>
-      <div className="cardContainer">
-        <motion.div className="card" whileHover={{ scale: 1.1 }}>
-          <Link href="/projects/Tre">
-            <a>
-              <img
-                src="https://raw.githubusercontent.com/JhsK/Tre/master/front/public/image/plan_page.png"
-                alt="cover"
-              />
-              <div className="cardTitleContainer">
-                <span>Tre</span>
-                <span>To Record Everything - 일정 관리 사이트</span>
-              </div>
-            </a>
-          </Link>
-        </motion.div>
-      </div>
-    </ProjectsContainer>
-    <ProjectsContainer>
-      <div className="titleContainer">
-        <GrGroup size="40" className="icons" />
-        <span className="ProjectsTitle">팀 프로젝트</span>
-      </div>
-      <div className="cardContainer">
-        <motion.div className="card" whileHover={{ scale: 1.1 }}>
-          <Link href="/projects/Groupstory">
-            <a>
-              <img
-                src="https://raw.githubusercontent.com/JhsK/JS-Groupstory/dbclass/public/image/aaaa.png"
-                alt="cover"
-              />
-              <div className="cardTitleContainer">
-                <span>Groupstory</span>
-                <span>백석대학교 동아리 모집 사이트</span>
-              </div>
-            </a>
-          </Link>
-        </motion.div>
-      </div>
-    </ProjectsContainer>
-  </Container>
-);
+const SwiperConatiner = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const ProjectsComponent = () => {
+  const isMobile = useDevice(768);
+
+  return (
+    <Container>
+      <ProjectsContainer>
+        <div className="titleContainer">
+          <BsPerson size="40" className="icons" />
+          <span className="ProjectsTitle">개인 프로젝트</span>
+        </div>
+        <div className="cardContainer">
+          {isMobile ? (
+            <SwiperConatiner>
+              <Swiper
+                modules={[Autoplay]}
+                loop
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+                }}
+              >
+                <SwiperSlide>
+                  <motion.div className="card" whileHover={{ scale: 1.1 }}>
+                    <Link href="/projects/Tre">
+                      <a>
+                        <img
+                          src="https://raw.githubusercontent.com/JhsK/Tre/master/front/public/image/plan_page.png"
+                          alt="cover"
+                        />
+                        <div className="cardTitleContainer">
+                          <span>Tre</span>
+                          <span>To Record Everything - 일정 관리 사이트</span>
+                        </div>
+                      </a>
+                    </Link>
+                  </motion.div>
+                </SwiperSlide>
+              </Swiper>
+            </SwiperConatiner>
+          ) : (
+            <motion.div className="card" whileHover={{ scale: 1.1 }}>
+              <Link href="/projects/Tre">
+                <a>
+                  <img
+                    src="https://raw.githubusercontent.com/JhsK/Tre/master/front/public/image/plan_page.png"
+                    alt="cover"
+                  />
+                  <div className="cardTitleContainer">
+                    <span>Tre</span>
+                    <span>To Record Everything - 일정 관리 사이트</span>
+                  </div>
+                </a>
+              </Link>
+            </motion.div>
+          )}
+        </div>
+      </ProjectsContainer>
+      <ProjectsContainer>
+        <div className="titleContainer">
+          <GrGroup size="40" className="icons" />
+          <span className="ProjectsTitle">팀 프로젝트</span>
+        </div>
+        <div className="cardContainer">
+          {isMobile ? (
+            <SwiperConatiner>
+              <Swiper
+                modules={[Autoplay]}
+                loop
+                autoplay={{
+                  delay: 2500,
+                  disableOnInteraction: false,
+                }}
+              >
+                <SwiperSlide>
+                  <motion.div className="card" whileHover={{ scale: 1.1 }}>
+                    <Link href="/projects/Groupstory">
+                      <a>
+                        <img
+                          src="https://raw.githubusercontent.com/JhsK/JS-Groupstory/dbclass/public/image/aaaa.png"
+                          alt="cover"
+                        />
+                        <div className="cardTitleContainer">
+                          <span>Groupstory</span>
+                          <span>백석대학교 동아리 모집 사이트</span>
+                        </div>
+                      </a>
+                    </Link>
+                  </motion.div>
+                </SwiperSlide>
+              </Swiper>
+            </SwiperConatiner>
+          ) : (
+            <motion.div className="card" whileHover={{ scale: 1.1 }}>
+              <Link href="/projects/Groupstory">
+                <a>
+                  <img
+                    src="https://raw.githubusercontent.com/JhsK/JS-Groupstory/dbclass/public/image/aaaa.png"
+                    alt="cover"
+                  />
+                  <div className="cardTitleContainer">
+                    <span>Groupstory</span>
+                    <span>백석대학교 동아리 모집 사이트</span>
+                  </div>
+                </a>
+              </Link>
+            </motion.div>
+          )}
+        </div>
+      </ProjectsContainer>
+    </Container>
+  );
+};
 
 export default ProjectsComponent;
