d68c4e986c415c0e632fb246d101a20d932cebe6
JhsK <qwe6293@nate.com>
2022-02-16 23:24:23 +0900

    [#23] 프로젝트 상세 페이지 반응형 적용

--- front/src/pages/projects/[id].tsx
+++ front/src/pages/projects/[id].tsx
@@ -7,8 +7,25 @@ import { getAllProjectsIds, getProjectsData } from 'src/lib/project';
 const Viewer = dynamic(() => import('../../components/Blog/Post/PostViewer'), { ssr: false });
 
 const Container = styled.div`
-  width: 1200px;
+  max-width: 1200px;
+  width: 100%;
   margin: 10rem auto 0 auto;
+
+  @media ${(props) => props.theme.HDPC} {
+    max-width: 1000px;
+  }
+
+  @media ${(props) => props.theme.PC} {
+    max-width: 700px;
+  }
+
+  @media ${(props) => props.theme.TABLET} {
+    max-width: 500px;
+  }
+
+  @media ${(props) => props.theme.MOBILE} {
+    max-width: 350px;
+  }
 `;
 
 export default function ProjectsItem({ projectsData }) {
