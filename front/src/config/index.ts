fd8d5e7c6fedf37100caac375efa0cc062148e92
JhsK <qwe6293@nate.com>
2022-03-08 21:39:45 +0900

    config env 수정

--- front/src/config/index.ts
+++ front/src/config/index.ts
@@ -1,6 +1,6 @@
 export const configs = {
-  API_URL: process.env.API_URL || 'http://localhost:3001',
-  SERVICE_URL: process.env.SERVICE_URL || 'http://localhost:3000',
+  API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
+  SERVICE_URL: process.env.NEXT_PUBLIC_SERVICE_URL || 'http://localhost:3000',
   ENV: process.env.NODE_ENV || 'development',
   VERSION: process.env.VERSION || '1',
 };
