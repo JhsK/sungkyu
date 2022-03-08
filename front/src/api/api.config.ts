013d34484d031863cea77118718ed85607590174
JhsK <qwe6293@nate.com>
2022-03-08 21:53:57 +0900

    api base url 수정

--- front/src/api/api.config.ts
+++ front/src/api/api.config.ts
@@ -6,7 +6,8 @@ const { API_URL, VERSION } = configs;
 axios.defaults.withCredentials = true;
 
 const headerConfig = {
-  baseURL: API_URL,
+  // baseURL: API_URL,
+  baseURL: 'http://13.125.205.121',
   headers: {
     'Content-Type': 'application/json',
     'Accept-Version': `v${VERSION}`,
