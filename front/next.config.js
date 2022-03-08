2b8c9a7c0e0d4c15da344a32dd931be280f1b15a
JhsK <qwe6293@nate.com>
2022-03-08 18:59:11 +0900

    build 파일 옵션 수정

--- front/next.config.js
+++ front/next.config.js
@@ -3,7 +3,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withBundleAnalyzer({
-  distDir: 'build',
   compress: true,
   webpack(config, { webpack }) {
     const prod = process.env.NODE_ENV === 'production';
