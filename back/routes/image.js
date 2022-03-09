0880d75db4d0b4dc52088954b3816846ed1b22bf
JhsK <qwe6293@nate.com>
2022-03-09 19:44:34 +0900

    s3 image 버그 수정

--- back/routes/image.js
+++ back/routes/image.js
@@ -52,7 +52,7 @@ const upload = multer({
 
 router.post("/", isLoggedIn, upload.single("img"), (req, res, next) => {
   // res.json(req.file.filename);
-  res.json(req.location);
+  res.json(req.file.location);
 });
 
 // router.post("/", isLoggedIn, upload.single("img"), async (req, res, next) => {
