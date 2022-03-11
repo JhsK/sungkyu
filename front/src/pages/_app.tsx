277a42219162fc57a8cb6a5d32aa813d07cb2370
JhsK <qwe6293@nate.com>
2022-03-11 11:51:28 +0900

    seo 위한 기본 메타 태그 설정

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -18,6 +18,18 @@ const App = ({ Component, pageProps }: AppProps) => {
     <>
       <Head>
         <meta charSet="utf-8" />
+        <meta name="description" content="프론트엔드 개발자 Sungkyu의 기술 블로그 입니다." />
+        <meta name="keywords" content="HTML, CSS, Javascript, React, Next, Frontend, Blog" />
+        <meta name="author" content="SungKyu" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://sungkyu.info" />
+        <meta property="og:title" content="Content Title" />
+        <meta property="og:image" content="https://github.com/JhsK/sungkyu/blob/master/front/public/profile.jpeg" />
+        <meta property="og:description" content="프론트엔드 개발자 Sungkyu의 기술 블로그 입니다." />
+        <meta property="og:site_name" content="Sungkyu" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:locale" content="ko_KR" />
         <meta
           name="viewport"
           content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=yes,viewport-fit=cover"
